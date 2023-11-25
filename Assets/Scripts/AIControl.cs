45e8c0c1c344199923ddd32268e6385a282e3641
inaciozao <126675642+inaciozao@users.noreply.github.com>
2023-11-25 14:57:03 +0000

    Tutorial Creating a City Crowd Terminado

--- Assets/Scripts/AIControl.cs
+++ Assets/Scripts/AIControl.cs
@@ -5,17 +5,32 @@ using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour {
 
-    public GameObject goal;
+    GameObject[] goalLocations;
     NavMeshAgent agent;
+    Animator anim;
 
     void Start() {
 
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(goal.transform.position);
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        int i = Random.Range(0, goalLocations.Length);
+        agent.SetDestination(goalLocations[i].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetTrigger("isWalking");
+        anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
+        float sm = Random.Range(0.5f, 2.0f);
+        anim.SetFloat("speedMult", sm);
+        agent.speed *= sm;
+
     }
 
 
     void Update() {
 
+        if (agent.remainingDistance < 1.0f) {
+
+            int i = Random.Range(0, goalLocations.Length);
+            agent.SetDestination(goalLocations[i].transform.position);
+        }
     }
 }
